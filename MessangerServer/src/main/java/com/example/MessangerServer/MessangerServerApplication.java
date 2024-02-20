1a6476d9f0303a51b609f6cf8b9e8331c87497b3
Ruslan Mukhamedov <lezeksrus@gmail.com>
2024-02-20 16:20:07 +0600

    Project has been initialized

---
+++ MessangerServer/src/main/java/com/example/MessangerServer/MessangerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MessangerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessangerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessangerServerApplication.class, args);
+	}
+
+}
